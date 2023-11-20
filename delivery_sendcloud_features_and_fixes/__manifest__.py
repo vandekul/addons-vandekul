2c2a8a75c43fdcecd0e77cfb2929d094aa37ac87
vandekul <susannafm@gmail.com>
2023-11-20 12:53:00 +0100

    SendCloud Integration Features and Bugs

--- delivery_sendcloud_features_and_fixes/__manifest__.py
+++ delivery_sendcloud_features_and_fixes/__manifest__.py
@@ -1,15 +1,14 @@
 # Copyright 2023 Vandekul (<https://github.com/vandekul>)
-# License OPL-1 (https://www.odoo.com/documentation/16.0/legal/licenses.html#odoo-apps).
 
 {
     "name": "Sendcloud Shipping Sending KIT products",
     "summary": "Compute shipping costs and ship with Sendcloud",
-    "images": ["static/description/logo-home.jpg"],
+    "images": ["static/description/bitmap1.png"],
     "category": "Operations/Inventory/Delivery",
     "version": "16.0",
-    "author": "Vande",
-    "license": "OPL-1",
+    "author": "Vandekul",
+    "license": "GPL-3",
+    "website": "https://github.com/vandekul",
     "depends": ["delivery_sendcloud_official"],
-    "data": ["security/ir.model.access.csv",
-             "views/sendcloud_integration_view.xml"],
+    "data": ["views/sendcloud_integration_view.xml"],
 }
