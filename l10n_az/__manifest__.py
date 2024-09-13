79b22aa2c864983efedc170c805b9f07dc71604f
vandekul <susannafm@gmail.com>
2024-09-13 11:35:37 +0200

    Localización rusia

---
+++ l10n_az/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014-2018 CodUP (<http://codup.com>).
+#
+##############################################################################
+
+{
+    'name': 'Azerbaijan - Accounting',
+    'version': '16.0',
+    'category': "Accounting/Localizations/Account Charts",
+    'description': """
+This is the base module to manage the accounting chart for Azerbaijan in Odoo.
+    """,
+    'author': 'CodUP',
+    'website': 'http://codup.com',
+    'depends': ['account'],
+    'demo': [],
+    'data': [
+        'data/account_chart.xml',
+        'data/account.account.template.csv',
+        'data/account_chart_template.xml',
+        'data/account_chart_template_data.xml',
+    ],
+    'sequence': 1,
+    'installable': True,
+}
